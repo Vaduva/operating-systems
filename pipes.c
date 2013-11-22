45c06de4effe9802db15bc0261cdbb46f220a2a9
Andrei Horak <linkyndy@gmail.com>
2013-11-22 11:33:35 +0200

    Updated file read block size
    Updated file read block size to filesystem's blocksize.

--- pipes.c
+++ pipes.c
@@ -6,90 +6,101 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <ctype.h>
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		printf("Not enough arguments\n");
-		exit(1);
-	}
+    if (argc != 2) {
+        printf("Not enough arguments\n");
+        exit(1);
+    }
 
-	int pip[2];
-	int pid;
+    int pip[2];
+    int pid;
 
-	// Create pipe
-	if (pipe(pip) == -1) {
-		printf("Pipe creation error\n");
-		exit(1);
-	}
+    // Create pipe
+    if (pipe(pip) == -1) {
+        printf("Pipe creation error\n");
+        exit(1);
+    }
 
-	// Create child process
-	pid = fork();
-	if (pid == -1) {
-		printf("Fork creation error\n");
-		exit(1);
-	}
+    // Create child process
+    pid = fork();
+    if (pid == -1) {
+        printf("Fork creation error\n");
+        exit(1);
+    }
 
-	// In child process
-	if (pid == 0) {
-		// Close write end of pipe
-		close(pip[1]);
+    // In child process
+    if (pid == 0) {
+        // Close write end of pipe
+        close(pip[1]);
 
-		char byte;
-		int digit_count = 0;
+        char byte;
+        int digit_count = 0;
 
-		// Read from pipe
-		while (read(pip[0], &byte, 1)) {
-			if (isdigit(byte)) {
-				digit_count++;
-			}
-		}
+        // Read from pipe
+        while (read(pip[0], &byte, 1)) {
+            if (isdigit(byte)) {
+                digit_count++;
+            }
+        }
 
-		// Close the other end too
-		close(pip[0]);
+        // Close the other end too
+        close(pip[0]);
 
-		// Exit with number of digits
-		exit(digit_count);
-	}
+        // Exit with number of digits
+        exit(digit_count);
+    }
 
-	// Close read end of pipe
-	close(pip[0]);
+    // Close read end of pipe
+    close(pip[0]);
 
-	// In parent process
-	int file = open(argv[1], O_RDONLY);
+    // In parent process
+    int file = open(argv[1], O_RDONLY);
 
-	if (file == -1) {
-		printf("Given file \"%s\" could not be opened\n", argv[1]);
-		exit(1);
-	}
+    if (file == -1) {
+        printf("Given file \"%s\" could not be opened\n", argv[1]);
+        exit(1);
+    }
 
-	char byte;
+    // Get filesystem blocksize
+    int block_size;
+    struct stat s;
+    if (stat(argv[1], &s) < 0) {
+        printf("Information for file \"%s\" could not be retrieved\n", argv[1]);
+        exit(1);
+    }
+    block_size = s.st_blksize;
 
-	// Read given file char by char
-	while (read(file, &byte, 1)) {
-		// Write each char to pipe
-		if (write(pip[1], &byte, 1) == -1) {
-			printf("Could not write char from file to pipe\n");
-			exit(1);
-		}
-	}
+    // Set number of chars to be read from file
+    char *bytes = malloc(block_size * sizeof(char));
 
-	// Close file
-	close(file);
-	
-	// Close the other end too
-	close(pip[1]);
+    // Read given file char by char
+    while (read(file, bytes, block_size)) {
+        // Write each char to pipe
+        if (write(pip[1], bytes, block_size) == -1) {
+            printf("Could not write char from file to pipe\n");
+            exit(1);
+        }
+    }
 
-	// Wait for child process to terminate
-	int status;
-	wait(&status);
+    // Close file
+    close(file);
 
-	if (WIFEXITED(status)) {
-		printf("Child process ended with: %d\n", WEXITSTATUS(status));
-	}
+    // Close the other end too
+    close(pip[1]);
 
-	return 0;
+    // Wait for child process to terminate
+    int status;
+    wait(&status);
+
+    if (WIFEXITED(status)) {
+        printf("Child process ended with: %d\n", WEXITSTATUS(status));
+    }
+
+    return 0;
 }
